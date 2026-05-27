438e7a52cd13ab8b156a2bbc29b672b7c7c188f1
KKnFinite <kknfinite@gmail.com>
2026-05-27 01:32:45 -0500

    Add database initialization script

---
+++ init_db.py
@@ -0,0 +1,11 @@
+from app import create_app
+from app.extensions import db
+from app.models import User
+
+
+app = create_app()
+
+
+with app.app_context():
+    db.create_all()
+    print("Database tables created.")
