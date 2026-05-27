3cb053edf7963bf3f30af414aa4cb43b234177f3
KKnFinite <kknfinite@gmail.com>
2026-05-27 02:03:50 -0500

    Add auth access decorators

---
+++ app/auth/decorators.py
@@ -0,0 +1,29 @@
+from functools import wraps
+
+from flask import flash, redirect, url_for
+from flask_login import current_user, login_required
+
+
+def role_required(*roles):
+    def decorator(view_func):
+        @wraps(view_func)
+        @login_required
+        def wrapped_view(*args, **kwargs):
+            if current_user.role in roles:
+                return view_func(*args, **kwargs)
+
+            flash("Access denied. Your account does not have permission to view that area.", "error")
+            return redirect(url_for("neomotherbrain.dashboard"))
+
+        return wrapped_view
+
+    return decorator
+
+
+def mfa_required(view_func):
+    @wraps(view_func)
+    def wrapped_view(*args, **kwargs):
+        # TODO: Enforce MFA verification here after MFA enrollment and challenge flows are built.
+        return view_func(*args, **kwargs)
+
+    return wrapped_view
